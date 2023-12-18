1c05f837fbb008233d88396d6aaefbf219c6d06a
danvld12 <149764518+danvld12@users.noreply.github.com>
2023-12-18 22:31:14 +0100

    Add files via upload

---
+++ VujicDavid/Properties/prijava.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VujicDavid.Properties
+{
+    public partial class prijava : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
